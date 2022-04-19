2699beac3fb3b8873a53d2c0e2e2c3c1b8f10c8f
Mohamed El Shawa <mmelshawa@gmail.com>
2022-04-19 12:44:15 +0200

    Header

---
+++ src/Components/Buttons/ButtonHeader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default function ButtonHeader() {
+  return (
+    <div className='navbar'>
+      <button onClick="Click me">Home</button>
+      <button onClick="Click me">Profile</button>
+      <button onClick="Click me">Portofolio</button>
+      <button onClick="Click me">Contact me</button>
+    </div>
+  )
+}
+
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import './Header.css';
+import profile from '../../Assets/Images/profile.png'
+// import ButtonHeader from '../Buttons/ButtonHeader';
 
 function Header() {
   return (
-    <div>Suck ma..</div>
+    <div className='header'>
+      <img className='profile' src={profile} alt="Mohamed El Shawa" />
+        <button>Home</button>
+        <button>Profile</button>
+        <button>Portofolio</button>
+        <button>Contact me</button>
+        {/* <ButtonHeader /> */}
+        {/* <ButtonHeader />
+        <ButtonHeader />
+        <ButtonHeader /> */}
+        
+        
+        </div>
     // <h1>My header</h1>
   )
 }
